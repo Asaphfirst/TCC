8f44c5e5dd7265031060ef02351fe4d7d40aed07
Robotics Team <asaphfirst@hotmail.com>
2017-09-26 20:57:58 +1000

    Localizando o centro do contorno

--- Codeblocks C/DysplayImage/main.cpp
+++ Codeblocks C/DysplayImage/main.cpp
@@ -24,6 +24,8 @@ int main(int argc, char** argv)
 	double minRatio = 0.0;  // default Double
 	double maxRatio = 1.5;  // default Double
 
+    int cx=0,cy=0;
+
     if (argc!=2)
     {
         printf("Usage: limiar <input_image>\n");
@@ -52,7 +54,6 @@ int main(int argc, char** argv)
 
     vector<vector<Point> > contours;
 
-
     findContours(limiar,contours,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE);
     vector<vector<Point> > hull(contours.size());
     Mat output = Mat::zeros(limiar.rows,limiar.cols,CV_8UC3);
@@ -65,77 +66,56 @@ int main(int argc, char** argv)
     }else{
         cout << "number of objects detected: " << contours.size() << endl;
     }
+
+    vector<Moments> mu(contours.size()); ///////
+
     RNG rng(0xffffffff); // random number generated
     for(int i=0;i<contours.size();i++) // Draw contours
     {
       Rect bb = boundingRect(Mat(contours[i]));
 
-      double area = contourArea(contours[i]);
-      convexHull(Mat(contours[i], true), hull[i]);
-      double solid = 100 * area / contourArea(hull[i]);
-      double ratio = (double) bb.width / (double) bb.height;
-
-
 			if (bb.width > minWidth && bb.width < maxWidth)
             {
-                cout << "the bb.width is: " << bb.width << endl;
+                //cout << "the bb.width is: " << bb.width << endl;
                 if (bb.height > minHeight && bb.height < maxHeight)
                 {
-                    cout << "the bb.height is: " << bb.height << endl;
+                    //cout << "the bb.height is: " << bb.height << endl;
+                    double area = contourArea(contours[i]);
                     if (area > minArea)
                     {
-                        cout << "the area is: " << area << endl;
+                        //cout << "the area is: " << area << endl;
+                        convexHull(Mat(contours[i], true), hull[i]);
+                        double solid = 100 * area / contourArea(hull[i]);
+
                         if (solid > solidity[0] && solid < solidity[1])
                         {
-                            cout << "the solidity is: " << solid << endl;
+                            //cout << "the solidity is: " << solid << endl;
+                            double ratio = (double) bb.width / (double) bb.height;
                             if (ratio > minRatio && ratio < maxRatio)
                             {
-                                cout << "the ratio is: " << ratio << endl;
+                                //cout << "the ratio is: " << ratio << endl;
                                 Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) ); // get a different color for each contour
-                                drawContours(output,contours,i, color);  // draw actual contour
+                                drawContours(output,contours,i, color);  // draw desired contour
                                 rectangle(output2,bb,color);
+
+
+                                mu[i] = moments(Mat(contours[i]));
+                                cx = int(mu[i].m10/mu[i].m00);
+                                cy = int(mu[i].m01/mu[i].m00);
                             }
                         }
                     }
                 }
             }
-
     }
+
     imshow("result",output);
     waitKey();
 
     imshow("result",output2);
     waitKey();
+
+    printf("posicao em x: %d posicao em y: %d", cx,cy);
     return 0;
 }
-/*
-        Rect bb = boundingRect(Mat(contours[i]));
-        //if (bb.width < minWidth || bb.width > maxWidth) continue;
-        //if (bb.height < minHeight || bb.height > maxHeight) continue;
-        double area = contourArea(contours[i]); // works
-        if (area > minArea){
-            cout << "area: " << area << endl;
-        }
-        convexHull(Mat(contours[i], true), hull[i]); // works
-        double solid = 100 * area / contourArea(hull[i]);// encontra a solidez do contorno works
 
-        cout << "Solidez: " << solid << endl;
-
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) ); // get a different color for each contour
-        drawContours(output,contours,i, color);  // draw actual contour
-        rectangle(output2,bb,color);
-        */
-/*
-            if (bb.width < minWidth || bb.width > maxWidth) continue;
-			if (bb.height < minHeight || bb.height > maxHeight) continue;
-			double area = contourArea(contours);
-			if (area < minArea) continue;
-			if (arcLength(contours, true) < minPerimeter) continue;
-			convexHull(Mat(contours, true), hull);
-			double solid = 100 * area / contourArea(hull);
-			if (solid < solidity[0] || solid > solidity[1]) continue;
-			if (contours.size() < minVertices || contours.size() > maxVertices)	continue;
-			double ratio = (double) bb.width / (double) bb.height;
-			if (ratio < minRatio || ratio > maxRatio) continue;
-			//output.push_back(contour);
-*/
